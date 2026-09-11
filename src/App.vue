2d489ce05453357e416ef2677b53bd7185c975af
Igor Kretov <knigor1337@gmail.com>
2026-09-11 11:46:03 +0300

    chore: Added theme mode

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,58 @@
 <template>
-  <nav>
-    <RouterLink to="/" class="text-red-400 hover:text-blue-400">
-      Home
-    </RouterLink>
+  <div class="flex flex-col min-h-dvh">
+    <header class="shrink-0 flex justify-end">
+      <NavigationMenu class="px-4 py-4 flex gap-2">
+        <NavigationMenuLink>
+          <RouterLink to="/">
+            Home
+          </RouterLink>
+        </NavigationMenuLink>
 
-    <RouterLink to="/projects" class="text-red-400 hover:text-blue-400">
-      Projects
-    </RouterLink>
-  </nav>
+        <NavigationMenuLink>
+          <RouterLink to="/projects">
+            Projects
+          </RouterLink>
+        </NavigationMenuLink>
+      </NavigationMenu>
 
-  <main>
-    <RouterView />
-  </main>
+      <Button
+        variant="outline"
+        size="icon"
+        @click="handleMode"
+      >
+        <Sun v-if="mode === 'dark'" />
+
+        <Moon v-else />
+      </Button>
+    </header>
+
+    <main class="flex-1 p-6">
+      <RouterView />
+    </main>
+  </div>
 </template>
 
 <script setup lang="ts">
+import {
+  NavigationMenu,
+  NavigationMenuLink,
+} from '@/components/ui/navigation-menu'
+import { Button } from '@/components/ui/button'
+
+import { useColorMode } from '@vueuse/core'
+
+import { Sun } from '@lucide/vue'
+import { Moon } from '@lucide/vue'
+
+const mode = useColorMode({ disableTransition: false })
+
+const handleMode = () => {
+  if (mode.value === 'dark') {
+    mode.value = 'light'
+  } else {
+    mode.value = 'dark'
+  }
+}
+
 </script>
 
